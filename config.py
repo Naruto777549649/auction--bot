cba104ce52acd07503d11084c1b1254ee68e7da2
Naruto777549649 <sikrar026@gmail.com>
2025-03-24 14:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
 from pyrogram import Client
 
-api_id = "YOUR_API_ID"
-api_hash = "YOUR_API_HASH"
-bot_token = "YOUR_BOT_TOKEN"
+api_id = os.environ.get("API_ID")
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 
-admins = [123456789, 987654321]  # Add Admin IDs
+admins = list(map(int, os.environ.get("ADMINS", "").split(",")))
 
 app = Client("auction_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
